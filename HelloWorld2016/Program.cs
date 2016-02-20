455f5ed120c357f8bfa99a06963987dcf70d2754
Your Name <you@example.com>
2016-02-20 13:15:41 -0500

    setting up repo

---
+++ HelloWorld2016/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloWorld2016
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World");
+
+            for (int i = 0; i  < 11; i++)
+            {
+                Console.WriteLine(i);
+            }
+        }
+    }
+}
